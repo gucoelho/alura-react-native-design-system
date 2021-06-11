34b1c49b086f3bfab2441d0f77049d8234c1c6d6
Gustavo Coelho <gustavo.coelho@lambda3.com.br>
2021-06-11 03:02:04 -0300

    Adiciona layout inicial do header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Image, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#F4F0F4'},
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 24,
+  },
+  bagContainer: {backgroundColor: 'white', padding: 18, borderRadius: 30},
+  title: {
+    fontFamily: 'OpenSans-ExtraBold',
+    fontSize: 28,
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
+});
 
 const App = () => (
-  <SafeAreaView style={{flex: 1, backgroundColor: '#F4F0F4'}}>
-    <View>
-      <Text>Lighteria</Text>
+  <SafeAreaView style={styles.container}>
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>Lighteria</Text>
+      <View style={styles.bagContainer}>
+        <Image
+          source={require('./assets/images/icone-sacola.png')}
+          style={styles.image}
+        />
+      </View>
     </View>
   </SafeAreaView>
 );
