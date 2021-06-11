6190b263d045b291f6940ad6fbe6a52b8b226417
Gustavo Coelho <gustavo.coelho@lambda3.com.br>
2021-06-11 01:56:32 -0300

    Setup inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <Text>Teste</Text>
+  </SafeAreaView>
+);
+
+export default App;
