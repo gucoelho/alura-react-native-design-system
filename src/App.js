ecda74bd7f5a98a8cf2b286949385d967d40c44b
Gustavo Coelho <gu_coelho011@hotmail.com>
2021-06-11 11:47:01 -0300

    01. Estilização do cabeçalho (#1)
    * Adiciona assets
    
    * Adiciona layout inicial do header
    
    * Adiciona estilização de divisor de Categoria
    
    Co-authored-by: Gustavo Coelho <gustavo.coelho@lambda3.com.br>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View, Text, Image, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#F4F0F4'},
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 24,
+  },
+  bagContainer: {backgroundColor: 'white', padding: 18, borderRadius: 50},
+  title: {
+    fontFamily: 'OpenSans-ExtraBold',
+    fontSize: 28,
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
+  divider: {
+    borderWidth: 0.5,
+    borderColor: '#A1A5AA'
+  },
+  dividerContainer: {
+    paddingHorizontal: 24
+  },
+  dividerLabelContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: -46
+  },
+  dividerLabel: {
+    padding: 34,
+    backgroundColor: '#F4F0F4',
+    fontSize: 16,
+    fontFamily: 'OpenSans-Regular',
+    color: '#A1A5AA'
+  }
+});
 
 const App = () => (
-  <SafeAreaView>
-    <Text>Teste</Text>
+  <SafeAreaView style={styles.container}>
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>Lighteria</Text>
+      <View style={styles.bagContainer}>
+        <Image
+          source={require('./assets/images/icone-sacola.png')}
+          style={styles.image}
+        />
+      </View>
+    </View>
+    <View style={styles.dividerContainer}>
+      <View style={styles.divider} />
+      <View style={styles.dividerLabelContainer}>
+        <Text style={styles.dividerLabel}>
+          Categoria
+        </Text>
+       </View>
+    </View>
   </SafeAreaView>
 );
 
